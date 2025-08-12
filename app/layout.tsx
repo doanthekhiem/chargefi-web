6c9a28245dc98afb5accaa003a1cb13436240d0d
Đoàn Thế Khiêm <44671735+doanthekhiem@users.noreply.github.com>
2025-08-12 21:59:12 +0700

    123

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,32 @@ const priego = localFont({
 export const metadata: Metadata = {
   title: "ChargeFi - Charge. Earn. Repeat.",
   description: "Turn Everyday Charging into Crypto Rewards. Earn $CHARGE Tokens By Simply Charging your Phone Or EV.",
+  openGraph: {
+    url: "https://www.chargefi.org/",
+    type: "website",
+    title: "ChargeFi - Charge. Earn. Repeat.",
+    description: "Turn Everyday Charging into Crypto Rewards. Earn $CHARGE Tokens By Simply Charging your Phone Or EV.",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/c15af51c-d291-482f-ad7d-cdbaa3e570fc.png?token=MfMLoIp7dnVExnPo62K37Hsxh39dGbV-F9ZMnW0j0Wg&height=1000&width=1000&expires=33291010606",
+        width: 1000,
+        height: 1000,
+        alt: "ChargeFi - Charge. Earn. Repeat.",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ChargeFi - Charge. Earn. Repeat.",
+    description: "Turn Everyday Charging into Crypto Rewards. Earn $CHARGE Tokens By Simply Charging your Phone Or EV.",
+    images: [
+      "https://opengraph.b-cdn.net/production/images/c15af51c-d291-482f-ad7d-cdbaa3e570fc.png?token=MfMLoIp7dnVExnPo62K37Hsxh39dGbV-F9ZMnW0j0Wg&height=1000&width=1000&expires=33291010606",
+    ],
+  },
+  other: {
+    "twitter:domain": "chargefi.org",
+    "twitter:url": "https://www.chargefi.org/",
+  },
 };
 
 export default function RootLayout({
